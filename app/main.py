758aa9568e653cdfe26d2190e5e7ecc5dd20bc1f
tjennings-pi <tamara.jennings@penn-interactive.com>
2022-08-12 11:04:56 -0400

    add router stuff, add test route

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+import fastapi
 
-from database import engine
+from database import engine, get_db
 import models
 from config import settings
+from routers import user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-()
+app.include_router(user.router) #include router object from user file, imports specific routes
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/test")
+def test_route():
+    return {"data"}
